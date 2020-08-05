14f4a2ab3f34ed7553c684ab52a231f16e6bf418
yushen1993 <1140656553@qq.com>
2020-08-05 13:29:18 +0800

    test

---
+++ PyCI/demo.py
@@ -0,0 +1,7 @@
+#-*- coding:utf-8 -*-
+# @Author : yushen
+# @Time : 2020-08-05 13:25
+def test_08_demo():
+    assert 3+5==9
+if __name__ == '__main__':
+    test_08_demo()
--- PyCI/runMain.py
+++ PyCI/runMain.py
@@ -1,9 +1,9 @@
 #-*- coding:utf-8 -*-
 # @Author : yushen
 # @Time : 2020-08-05 11:51
-from gitnews import *
+from demo import *
 import os
 path = os.path.split(os.path.realpath(__file__))[0]
 
 if __name__ == '__main__':
-    git_news()
+    test_08_demo()
