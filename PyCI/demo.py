9773b9b5793c0407b0dba0b8fe175edf50f3a437
yushen1993 <1140656553@qq.com>
2020-08-05 13:33:15 +0800

    test

--- PyCI/demo.py
+++ PyCI/demo.py
@@ -1,7 +1,11 @@
 #-*- coding:utf-8 -*-
 # @Author : yushen
 # @Time : 2020-08-05 13:25
+import allure
+
+@allure.testcase('持续集成-测试断言')
 def test_08_demo():
-    assert 3+5==9
+    assert 3+5==8
+    print("测试通过")
 if __name__ == '__main__':
     test_08_demo()
