97bd73032ba7141091b073722508bd48fe54a795
yushen1993 <1140656553@qq.com>
2020-08-07 10:56:19 +0800

    newsadd

---
+++ gitNews/runMain.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 -*-
+# @Author : yushen
+# @Time : 2020-08-05 11:51
+
+import sys
+
+sys.path.append(r'E:\yushenTools\SFLdata\othersdemo')
+
+from gitnews import *
+
+
+if __name__ == '__main__':
+    git_news()
